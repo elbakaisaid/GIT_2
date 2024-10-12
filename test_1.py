d7baaa12076ad22e69af0fe89da3d4944d39630a
saidelbakai <saidelbakai1985@gmail.com>
2024-10-12 18:36:45 +0200

    the therth commit in feature 2

--- test_1.py
+++ test_1.py
@@ -1,2 +1,3 @@
 print("welcome to feature 2 branch from python languege!\n")
 print("This is the second commit in the feature 2 branch from python!\n")
+print("This is the therth commit in the feature 2 branch from python!\n")
--- test_2.c
+++ test_2.c
@@ -4,4 +4,5 @@
 int main() {
   printf("Welcome to feature 2 branch from c languege!\n");
   printf("This is the second commit in feature 2 branch\n");
+  printf("This is the therth commit in feature 2 branch\n");
 }
