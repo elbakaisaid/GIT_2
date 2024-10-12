f588d6cb78be97dac36ab7496aad835c5c2ca954
saidelbakai <saidelbakai1985@gmail.com>
2024-10-12 19:03:01 +0200

    The first commit in featur 4

--- test_2.c
+++ test_2.c
@@ -3,10 +3,11 @@
 
 int main() {
   printf("Welcome to feature 1 branch from c languege!\n");
-  printf("this is the second commit in feature 1 branch!\n");
+  printf("this is the second commit in feature 1 branch!\nbr");
   printf("Welcome to feature 2 branch from c languege!\n");
   printf("This is the second commit in feature 2 branch\n");
   printf("This is the therth commit in feature 2 branch\n");
   printf("print the fist commit in feature 3 branch!\n");
   printf("print the second commit in feature 3 branch!\n");
+  printf("print the fist commit in feature 4 branch!\n");
 }
