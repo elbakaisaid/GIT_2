ec601c6b2616372f470b1ed50754151d250ac7ac
saidelbakai <saidelbakai1985@gmail.com>
2024-10-12 18:28:00 +0200

    second commit in feature 1 branch

--- test_1.py
+++ test_1.py
@@ -1,1 +1,2 @@
 print("welcome in feature 1 branch from python languege!")
+print("this is the second commit in feature 1 branch!")
--- test_2.c
+++ test_2.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() { printf("Welcome to feature 1 branch from c languege!\n"); }
+int main() {
+  printf("Welcome to feature 1 branch from c languege!\n");
+  printf("this is the second commit in feature 1 branch!");
+}
